931186c52b9e1da75d6ddb141b4e7fa8112c7191
DESKTOP-CJOERL0\LENOVO <cecepns0429@gmail.com>
2021-06-16 13:29:24 +0700

    update active class navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,14 +11,28 @@ export class NavbarComponent implements OnInit {
 
   
   ngOnInit(): void {
+    
+    let WrapperBottomAboutMe = window.document.getElementById('aboutme')?.offsetHeight || '',
+        WrapperTopFasilitas = window.document.getElementById('fasilitas')?.offsetTop || '',
+        WrapperBottomFasilitas = window.document.getElementById('fasilitas')?.offsetHeight || '';
 
     let prevScrollpos = window.pageYOffset;
       window.onscroll = ()=> {
       let currentScrollPos = window.pageYOffset;
+      console.log(currentScrollPos)
         if (prevScrollpos > currentScrollPos) {
-          window.document.getElementById('navbar')?.classList.remove('is-scrolled');
+          document.getElementById('navbar')?.classList.remove('is-scrolled');
+          if (prevScrollpos >= 0 && prevScrollpos <= WrapperBottomAboutMe  ) {
+            document.getElementById('link-aboutme')?.classList.add('is-active')
+          } else {
+            document.getElementById('link-aboutme')?.classList.remove('is-active')
+          } if(prevScrollpos >= WrapperTopFasilitas ) {
+            document.getElementById('link-fasilitas')?.classList.add('is-active')
+          } else {
+            document.getElementById('link-fasilitas')?.classList.remove('is-active')
+          }
         } else {
-        window.document.getElementById('navbar')?.classList.add ('is-scrolled');
+          window.document.getElementById('navbar')?.classList.add ('is-scrolled');
 
         }
         prevScrollpos = currentScrollPos;
